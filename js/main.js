4b88b59fa19fd41e4ba4024769244a0577b9b5e0
Roma Shepida <roma.shepida@gmail.com>
2020-12-01 00:12:09 +0200

    section ABOUT done, slider done

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
-$(function(){
-
+$(function () {
+    $('.slider__items').slick({
+        infinite: true,
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        arrows: false,
+        // autoplay: true,
+        autoplaySpeed: 4000,
+        dots: true,
+    });
 });
