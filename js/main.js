d01705ab7663a7aa5fcd74ef042614cbab8e4a90
Roma Shepida <roma.shepida@gmail.com>
2021-02-12 17:59:28 +0200

    configured menu button in HEADER (media-queries)

--- js/main.js
+++ js/main.js
@@ -4,8 +4,12 @@ $(function () {
         slidesToShow: 2,
         slidesToScroll: 1,
         arrows: false,
-        // autoplay: true,
+        autoplay: true,
         autoplaySpeed: 4000,
         dots: true,
+    });
+
+    $('.header-nav__button').on('click', function(){
+        $('.header-nav__list').slideToggle();
     });
 });
